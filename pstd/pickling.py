b1e5bdff61f40050b294b82eee205936f5e2a82c
Carter Hinsley <kvazau@protonmail.com>
2019-01-03 10:39:38 -0500

    Debug pickling module

--- pstd/pickling.py
+++ pstd/pickling.py
@@ -2,15 +2,17 @@ import pickle
 
 def load_state(trainee_name, training_cycle_id=""):
     try:
-        return pickle.load(open(
+        with open(
             "{} - {}.pickle".format(trainee_name, training_cycle_id),
             "rb"
-        ))
+        ) as file:
+            return pickle.load(file)
     except FileNotFoundError:
         return False
 
 def save_state(iter, trainee_name, training_cycle_id=""):
-    pickle.dump(iter, open(
-        "{} - {}.pickle".format(trainee_name, training_cycle_id),
+    with open(
+        "{}{}.pickle".format(trainee_name, training_cycle_id),
         "wb"
-    ))
+    ) as file:
+        pickle.dump(iter, file)
