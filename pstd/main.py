ab487eedfd370cd9d8cf289fb664e87071d0c7a9
Carter Hinsley <kvazau@protonmail.com>
2019-01-03 09:53:04 -0500

    Rename generator to iterator

--- pstd/main.py
+++ pstd/main.py
@@ -7,9 +7,9 @@ if debug:
     userio.print_training_cycle_config(sessions.default_config)
 
 def mainloop(config, debug=False):
-    generator = sessions.SessionBuilderCallbackIterator(config, debug)
+    iterator = sessions.SessionBuilderCallbackIterator(config, debug)
     while True:
-        session_builder = next(generator)
+        session_builder = next(iterator)
         session = session_builder(userio.get_fatigue_rating(),
                                   userio.get_training_max())
         userio.print_training_session(session)
