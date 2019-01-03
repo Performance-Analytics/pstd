60009c17c1b407b6987f5f1195672bd17b12a3db
Carter Hinsley <kvazau@protonmail.com>
2019-01-03 10:04:35 -0500

    Better pickle filename formatting

--- pstd/pickling.py
+++ pstd/pickling.py
@@ -2,9 +2,15 @@ import pickle
 
 def load_state(trainee_name, training_cycle_id=""):
     try:
-        return pickle.load(open(trainee_name + training_cycle_id, "rb"))
+        return pickle.load(open(
+            "{} - {}.pickle".format(trainee_name, training_cycle_id),
+            "rb"
+        ))
     except FileNotFoundError:
         return False
 
 def save_state(iter, trainee_name, training_cycle_id=""):
-    pickle.dump(iter, open(trainee_name + training_cycle_id, "wb"))
+    pickle.dump(iter, open(
+        "{} - {}.pickle".format(trainee_name, training_cycle_id),
+        "wb"
+    ))
