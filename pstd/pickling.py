a88f021b7f8293f9ea497d62b98a86e951c44b7a
Carter Hinsley <kvazau@protonmail.com>
2019-01-03 10:43:36 -0500

    Return None instead of False

--- pstd/pickling.py
+++ pstd/pickling.py
@@ -8,7 +8,7 @@ def load_state(trainee_name, training_cycle_id=""):
         ) as file:
             return pickle.load(file)
     except FileNotFoundError:
-        return False
+        return None
 
 def save_state(iter, trainee_name, training_cycle_id=""):
     with open(
